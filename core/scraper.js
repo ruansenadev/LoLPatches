a985ac04eb3ced55ffb90bcb495ef3506eff39ed
RuanSena <ruansenadev@gmail.com>
2020-04-08 16:03:36 -0300

    patches local

--- core/scraper.js
+++ core/scraper.js
@@ -9,12 +9,22 @@ const baseURL = 'https://br.leagueoflegends.com/pt-br/news/game-updates'
 // axios.defaults.baseURL = baseURL
 // axios.defaults.headers.common['User-Agent'] = chromeW_Ua
 
+// path para salvar json
+const localPatches = './patches.json'
+
 // page structure
 // const CONTENT = "[class*='Body']";
 // const BODYCONTAINER = "#___gatsby";
 const LOAD = "[class*='LoadMoreButton']"
 
-var ATTS = []
+var localATTS = []
+var lastATT
+var ATTS
+if (fs.existsSync(localPatches)) {
+	localATTS = JSON.parse(fs.readFileSync(localPatches, 'utf-8'))
+	lastATT = localATTS[0]
+}
+
 ;(async () => {
 	const brow = await pupp.launch({ headless: false })
 	const [pagina] = await brow.pages()
@@ -29,47 +39,74 @@ var ATTS = []
 			// abre atualizacoes
 			body.querySelector('ol').querySelector('li:nth-child(2) button').click()
 		})
-		
-		async function scraperAtts(atts = []) {
-			async function fetcher() {
-				// tempo que espero q carregue as imagens dsgrç
-				await pagina.waitFor(5000)
 
-				let agg = await pagina.$$eval('a', (atts) =>
-					atts.map((att) => {
-						let autor = att.querySelector("[class*='Author']")
-						let prev = {
-							uri: att.href,
-							img: att.querySelector('img').src,
-							titulo: att.querySelector('h2').innerText,
-							autor: autor ? autor.innerText : "",
-							data: att.querySelector('time').dateTime,
-						}
-						att.remove()
-						return prev
-					})
-				)
-				// so para de concatenar quando nao tiver mais o botao taok
-				if((await pagina.$(LOAD)) == null) {
-					return agg
-				} else {
-					await pagina.click(LOAD)
-					await pagina.waitForSelector('a')
-					// concatena com retorno nova chamada de fetcher
-					return agg.concat(await fetcher())
+		async function fetcher() {
+			// tempo para req ajax
+			await pagina.waitFor(5000)
+
+			let agg = await pagina.$$eval('a', (atts) =>
+				atts.map((att) => {
+					let autor = att.querySelector("[class*='Author']")
+					let prev = {
+						uri: att.href,
+						img: att.querySelector('img').src,
+						titulo: att.querySelector('h2').innerText,
+						autor: autor ? autor.innerText : '',
+						data: att.querySelector('time').dateTime,
+					}
+					att.remove()
+					return prev
+				})
+			)
+			if (lastATT) {
+				for (let i = 0; i < agg.length; i++) {
+					if (agg[i].data === lastATT.data) {
+						agg = agg.slice(0, i)
+						break
+						// encontrou ultima att entao divide ate antes dela e retorna
+					}
 				}
+				return agg
 			}
-			return atts.concat(await fetcher())
+			if ((await pagina.$(LOAD)) == null) {
+				// se chegou no fim e sua queue não estourou parabains o.o
+				return agg
+			} else {
+				await pagina.click(LOAD)
+				await pagina.waitForSelector('a')
+				// concatena com retorno da nova chamada de fetcher
+				return agg.concat(await fetcher())
+			}
 		}
 
-		ATTS = await scraperAtts()
-		
+		ATTS = await fetcher()
 	} catch (error) {
 		brow.close()
 		console.error(error)
 	} finally {
-		fs.writeFileSync('patches.json', JSON.stringify(ATTS, null, 2), 'utf-8')
-		console.log(ATTS)
+		if (ATTS.length) {
+			console.log(ATTS.length + ' novos patches')
+			ATTS = ATTS.concat(localATTS)
+			lastATT = ATTS[0]
+			fs.writeFile(
+				localPatches,
+				JSON.stringify(ATTS, null, 2),
+				'utf-8',
+				(e) => {
+					if (e) {
+						console.error(e)
+						console.log('Falha ao tentar escrever novos patches :|')
+					} else {
+						console.log(`Atualizado patches em ${localPatches} com sucesso ^-^`)
+					}
+				}
+			)
+		} else {
+			console.log('Aparentemente você já tem todos patches salvos, nice!')
+		}
+		console.log(
+			`\n Data do último patch: ${new Date(lastATT.data).toLocaleDateString()}`
+		)
 	}
 })()
 
