7cd1085fa9eb3fa4e93fea33b48c14a953d4864b
RuanSena <ruansenadev@gmail.com>
2020-04-28 14:45:52 -0300

    y axis text concatenate

--- core/vision.js
+++ core/vision.js
@@ -1,14 +1,15 @@
 const vision = require('@google-cloud/vision')
 
 // const imageFt = 'imagens/testVision.jpg'
-// const imageFt = 'imagens/inlineVision.jpg'
-const imageFt = 'imagens/noTitleVision.jpg'
+const imageFt = 'imagens/inlineVision.jpg'
+// const imageFt = 'imagens/noTitleVision.jpg'
 
 const client = new vision.ImageAnnotatorClient()
-const margin = 16;
+const marginX = 16;
+const marginY = 12;
 
 function isInline(texts = [], titles = {}) {
-    if(!Object.keys(titles).length) {
+    if (!Object.keys(titles).length) {
         return null
     }
     let iTitles = []
@@ -35,11 +36,11 @@ function formatInline(textArray, iTitles, detections) {
             if (txt.includes(d.description)) {
                 if (iTitles.includes(txt)) {
                     // left X vertice - m
-                    txts.push([txt, d.boundingPoly.vertices[0].x - margin])
+                    txts.push([txt, d.boundingPoly.vertices[0].x - marginX])
                     return txts
                 }
                 // right X vertice + m
-                txts.push([txt, d.boundingPoly.vertices[1].x + margin])
+                txts.push([txt, d.boundingPoly.vertices[1].x + marginX])
                 return txts
             }
         }
@@ -61,6 +62,52 @@ function mapFeatured(array, ft) {
     })
     return ft
 }
+// compare Y axis of items them join if its near
+function joinY(array, detections) {
+    let phraseBox
+    return array.reduce((txts, txt) => {
+        let txtBox
+        // word with a space followed by anything gets box
+        if (!(/(\w.+\s).+/i.test(txt))) {
+            txts.push(txt)
+            return txts
+        } else {
+            let lastInclude = 0
+            detections.forEach((d, i) => {
+                // map txt box
+                if (txt.includes(d.description)) {
+                    let box = d.boundingPoly.vertices
+                    // [{lx, ty}, {rx, ty}, {rx, by}, {lx,by}]
+                    if (txt.startsWith(d.description)) {
+                        txtBox = box;
+                        lastInclude = i
+                        return;
+                    } else if(lastInclude && lastInclude == (i - 1)) {
+                        // expands box X
+                        txtBox = [txtBox[0], box[1], box[2], txtBox[3]]
+                        lastInclude = i
+                    }
+                }
+            })
+
+            let joining = false
+            let midPoint = phraseBox ? phraseBox[3].x + (Math.round((phraseBox[2].x - phraseBox[3].x) / 2)) : null
+            // same Y column
+            if (midPoint && (midPoint > txtBox[0].x && midPoint < txtBox[1].x)) {
+                // margin Y matches concats
+                if ((txtBox[1].y - phraseBox[2].y) <= marginY) {
+                    txts[txts.length - 1] += ' ' + txt
+                    joining = true
+                }
+            }
+            // starts new sentence
+            if (!joining) txts.push(txt)
+            phraseBox = txtBox
+
+            return txts
+        }
+    }, [])
+}
 
 async function extractData(img) {
     const [result] = await client.textDetection(img)
@@ -72,6 +119,9 @@ async function extractData(img) {
             textArray.splice(i, 1)
         }
     })
+    console.log(textArray)
+    // concats Y near prhases
+    textArray = joinY(textArray, detections)
     // list titles
     let featured = textArray.filter((txt) => {
         return /[A-Z]{2,}/.test(txt)
@@ -79,7 +129,7 @@ async function extractData(img) {
         tts[tt.toLowerCase()] = []
         return tts
     }, {})
-    
+
     // filter X axis titles
     let inlineTitles = isInline(textArray, featured)
     if (inlineTitles) {
