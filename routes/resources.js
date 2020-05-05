a5c5e0024efeafe9d3eea97cb13117f228c38d4f
RuanSena <ruansenadev@gmail.com>
2020-05-05 11:28:55 -0300

    pagination control

--- routes/resources.js
+++ routes/resources.js
@@ -17,8 +17,19 @@ var corsOptions = {
 
 router.use(cors(corsOptions))
 
-router.get('/', function(req, res, next) {
+function paginate(data, page, items) {
+    // index of last item, not included in array slicing, means has more
+    let left = data[items*page] ? true : false
+    // if hasnt page items slice returns an empty array
+    return {items: data.slice(page==1?0:items*(page-1), items*(page)), left}
+}
+
+router.get('/patches', function(req, res, next) {
+    let page = req.query.page ? Number(req.query.page) : 1
+    let items = req.query.items ? Number(req.query.items) : 4
     let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../patches', 'data.json'), 'utf8'))
+    data = paginate(data.items, page, items)
+    console.log(data)
     res.json(data)
 })
 
