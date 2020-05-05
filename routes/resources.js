3f317b20ca6af41545a713a0e26daff325ba65a7
RuanSena <ruansenadev@gmail.com>
2020-05-05 18:30:51 -0300

    cors patch

--- routes/resources.js
+++ routes/resources.js
@@ -23,8 +23,9 @@ function paginate(data, page, items) {
     // if hasnt page items slice returns an empty array
     return {items: data.slice(page==1?0:items*(page-1), items*(page)), left}
 }
-
+// patches preview
 router.get('/patches', function(req, res, next) {
+    // sanitize page, items
     let page = req.query.page ? Number(req.query.page) : 1
     let items = req.query.items ? Number(req.query.items) : 4
     let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../patches', 'data.json'), 'utf8'))
@@ -32,4 +33,19 @@ router.get('/patches', function(req, res, next) {
     res.json(data)
 })
 
+// patch version
+router.get('/patch', function(req, res, next) {
+    // sanitize version
+    let v = req.query.version
+    if(!v) {
+        return next(new Error('Version not specified'))
+    }
+    fs.access(path.join(__dirname, '../patches', v), (err) => {
+        if(err) {
+            return next(new Error('Incorrect version'))
+        }
+        let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../patches', v, 'data.json'), 'utf8'))
+        res.json(data)
+    })
+})
 module.exports = router
